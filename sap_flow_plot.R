6a452d4222762900cafa493781bb68a486c3edd3
Elena Forbath <elenaforbath@Elenas-MacBook-Pro.local>
2018-06-14 11:07:05 -0400

    added new column with doy and hour

--- sap_flow_plot.R
+++ sap_flow_plot.R
@@ -2,4 +2,4 @@
 os.flag <- 2
 source("/Users/elenaforbath/Documents/GitHub/bewkes_field/sapflux_process.r")
 metA <- read.csv("/Volumes/data/data_repo/field_data/bewkes/sensor/decagon/met_air.csv")
-plot(sapflowF$doy, sapflowF$sapflowF1, type = "p")
+plot(sapflowF$doy, sapflowF$sapflowF1, type = "l")
